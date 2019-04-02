a4ed41ba9be978af133e77801701401cac2131ed
Botche <gabi_petkov@abv.bg>
2019-04-02 10:24:49 +0300

    Added Classes And Objects

---
+++ TechnologyFundamentalsC#/ClassesAndObjects/Students/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string[] input = Console.ReadLine()
+                .Split();
+            while (input[0] != "end")
+            {
+                string name = input[0];
+                string familyName = input[1];
+                int age = int.Parse(input[2]);
+                string town = input[3];
+                foreach (var sstudent in students)
+                {
+                    if (sstudent.Name == name && sstudent.FamilyName == familyName)
+                    {
+                        students.Remove(sstudent);
+                        break;
+                    }
+                }
+                Student student = new Student();
+
+                student.Name = name;
+                student.FamilyName = familyName;
+                student.Age = age;
+                student.Town = town;
+
+                students.Add(student);
+
+                input = Console.ReadLine()
+                .Split();
+            }
+            string filter = Console.ReadLine();
+            List<Student> filtered = students.
+                    Where(x => x.Town == filter)
+                    .ToList();
+            foreach (var student in filtered)
+            {
+                Console.WriteLine($"{student.Name} {student.FamilyName} is {student.Age} years old.");
+            }
+        }
+        class Student
+        {
+            public string Name;
+            public string FamilyName;
+            public int Age;
+            public string Town { get; set; }
+        }
+    }
+}
---
+++ TechnologyFundamentalsC#/ClassesAndObjectsMoreExe/OldestFamilyMember/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OldestFamilyMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int familyMembers = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            
+            for (int i = 0; i < familyMembers; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                Person person = new Person();
+                person.Name = input[0];
+                person.Age = int.Parse(input[1]);
+
+                people.Add(person);
+            }
+            var oldest = people.OrderByDescending(x => x.Age).ToList();
+            Console.WriteLine(oldest[0].Name + " " + oldest[0].Age);
+        }
+    }
+
+    class Person
+    {
+        public string Name;
+        public int Age;
+    }
+}
