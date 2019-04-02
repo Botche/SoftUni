a32f39de1adebdbbb764d1c2955289eea1e97444
Botche <gabi_petkov@abv.bg>
2019-04-02 08:33:03 +0300

    More

---
+++ TechnologyFundamentalsC#/BasicSyntaxExercise/Login/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string correctPass = string.Empty;
+            int br = 0;
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                correctPass += username[i];
+            }
+            string pass = Console.ReadLine();
+            while (true)
+            {
+                if (pass == correctPass)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else
+                {
+                    br++;
+                    if (br == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        break;
+                    }  
+                    Console.WriteLine("Incorrect password. Try again.");
+                    pass = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
