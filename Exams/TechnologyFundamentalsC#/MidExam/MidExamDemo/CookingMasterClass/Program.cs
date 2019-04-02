87e50d3b1df86022bf69d2787ee1374fe88f249a
Botche <gabi_petkov@abv.bg>
2019-04-02 10:39:26 +0300

    Added Mid Exams

---
+++ Exams/TechnologyFundamentalsC#/MidExam/MidExamDemo/CookingMasterClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CookingMasterClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double priceOfFloor = double.Parse(Console.ReadLine());
+            double priceOfEggs = double.Parse(Console.ReadLine());
+            double priceOfApron = double.Parse(Console.ReadLine());
+
+            double sumOfFloor = priceOfFloor * (students - students / 5);
+            double sumOfEggs = priceOfEggs * students *10;
+            double sumOfApron = priceOfApron * Math.Ceiling(students+students*0.20);
+            double sum = sumOfApron + sumOfEggs + sumOfFloor;
+            if (sum <= budget)
+            {
+                Console.WriteLine($"Items purchased for {sum:f2}$.");
+            }
+            else
+                Console.WriteLine($"{sum-budget:f2}$ more needed.");
+        }
+    }
+}
---
+++ Exams/TechnologyFundamentalsC#/MidExam/RetakeMidExamDec/SantasList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SantasList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> santasList = Console.ReadLine().Split("&").ToList();
+
+            string[] commands = Console.ReadLine().Split();
+            while (commands[0]!="Finished!")
+            {
+                string command = commands[0];
+                string firstKid = commands[1];
+
+                switch(command)
+                {
+                    case "Bad":
+                        if (!santasList.Contains(firstKid))
+                            santasList.Insert(0, firstKid);
+                        break;
+                    case "Good":
+                        if (santasList.Contains(firstKid))
+                            santasList.Remove(firstKid);
+                        break;
+                    case "Rename":
+                        string secondKid = commands[2];
+                        if (santasList.Contains(firstKid))
+                        {
+                            int index = santasList.IndexOf(firstKid);
+                            santasList.Remove(firstKid);
+                            santasList.Insert(index, secondKid);
+                        }
+                        break;
+                    case "Rearrange":
+                        if (santasList.Contains(firstKid))
+                        {
+                            santasList.Remove(firstKid);
+                            santasList.Add(firstKid);
+                        }
+                        break;
+                }
+
+                commands = Console.ReadLine().Split();
+            }
+            Console.WriteLine(string.Join(", ",santasList));
+        }
+    }
+}
