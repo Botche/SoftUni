49865cc81f06333e5def74846e0e194b1e6360c9
Botche <gabi_petkov@abv.bg>
2019-04-02 08:21:42 +0300

    Added Homework

---
+++ TechnologyFundamentalsC#/BasicSystax/Passed/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace passed
+{
+    class Program
+    {
+        static void Main()
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if(grade>=3)
+                Console.WriteLine("Passed!");
+
+        }
+    }
+}
