9e562c3fa530ef9602c267e09c78528dce109d9b
Botche <gabi_petkov@abv.bg>
2019-04-07 22:29:10 +0300

    Added FinalExam

---
+++ Exams/TechnologyFundamentalsC#/FinalExam/FinalExam4Dec/Skeleton-C#/BandRegister/Controllers/BandController.cs
@@ -0,0 +1,88 @@
+﻿using BandRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+using BandRegister.Data;
+using BandRegister.Models;
+using System.Linq;
+
+namespace BandRegister.Controllers
+{
+    public class BandController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db=new BandContext())
+            {
+                var allBands = db.bands.ToList();
+                return View(allBands);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Band band)
+        {
+            using (var db=new BandContext())
+            {
+                db.bands.Add(band);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db=new BandContext())
+            {
+                var bandToEdit = db.bands.Find(id);
+                if(bandToEdit==null)
+                    return RedirectToAction("Index");
+                return View(bandToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Band band)
+        {
+            using (var db = new BandContext())
+            {
+                var bandToEdit = db.bands.FirstOrDefault(x => x.Id == band.Id);
+                bandToEdit.Name = band.Name;
+                bandToEdit.Members = band.Members;
+                bandToEdit.Honorarium = band.Honorarium;
+                bandToEdit.Genre = band.Genre;
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new BandContext())
+            {
+                var bandToDelete = db.bands.Find(id);
+                if (bandToDelete == null)
+                    return RedirectToAction("Index");
+                return View(bandToDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Band band)
+        {
+            using (var db=new BandContext())
+            {
+                db.bands.Remove(band);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Exams/TechnologyFundamentalsC#/FinalExam/FinalExam4Dec/SongEncryption/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SongEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            
+            Regex pattern = new Regex(@"^(([A-Z][a-z' ]+):([A-Z ]+))");
+
+            while (input != "end")
+            {
+                bool match = pattern.IsMatch(input);
+                if (match == true)
+                {
+                    string[] data = input.Split(":");
+                    StringBuilder sb = new StringBuilder();
+                    string artist = data[0];
+                    string album = data[1];
+
+                    int keyToEncrypt = artist.Length;
+                    for (int i = 0; i < artist.Length; i++)
+                    {
+                        char ch = artist[i];
+                        if (ch == ' ' || ch == '\'')
+                        {
+                            sb.Append(ch);
+                        }
+                        else if (ch > 96)
+                        {
+                            if (ch + keyToEncrypt > 122)
+                            {
+                                ch = (char)(ch + keyToEncrypt - 122 + 96);
+                                sb.Append(ch);
+                            }
+                            else
+                            {
+                                ch = (char)(ch + keyToEncrypt);
+                                sb.Append(ch);
+                            }
+                        }
+                        else
+                        {
+                            if (ch + keyToEncrypt > 90)
+                            {
+                                ch = (char)(ch + keyToEncrypt - 90 + 64);
+                                sb.Append(ch);
+                            }
+                            else
+                            {
+                                ch = (char)(ch + keyToEncrypt);
+                                sb.Append(ch);
+                            }
+                        }
+                    }
+                    sb.Append('@');
+                    for (int i = 0; i < album.Length; i++)
+                    {
+                        char ch = album[i];
+                        if (ch == ' ' || ch == '\'')
+                        {
+                            sb.Append(ch);
+                        }
+                        else if (ch + keyToEncrypt > 90)
+                        {
+                            ch = (char)(ch + keyToEncrypt - 90 + 64);
+                            sb.Append(ch);
+                        }
+                        else
+                        {
+                            ch = (char)(ch + keyToEncrypt);
+                            sb.Append(ch);
+                        }
+                    }
+                    Console.WriteLine($"Successful encryption: {sb}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
