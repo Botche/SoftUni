0bf208eef9eda2c51000e13fadc4584958d70eb3
Botche <gabi_petkov@abv.bg>
2019-04-07 14:32:42 +0300

    Added WebBasics

---
+++ TechnologyFundamentalsC#/WebBasics/Phonebook/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Phonebook.Data;
+using Phonebook.Data.Models;
+
+namespace Phonebook.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create(Contacts contact)
+        {
+            DataAccess.Contacts.Add(contact);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
