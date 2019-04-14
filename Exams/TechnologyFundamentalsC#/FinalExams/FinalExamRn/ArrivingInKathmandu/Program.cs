65e2e979d4175e0d980a12d8bb0a30158e5d9051
Botche <gabi_petkov@abv.bg>
2019-04-14 11:20:38 +0300

    Added Final Exam

---
+++ Exams/TechnologyFundamentalsC#/FinalExams/FinalExamRn/ArrivingInKathmandu/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ArrivingInKathmandu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex pattern = new Regex(@"^(?<peakName>[A-Za-z0-9#!?@$]+)=(?<length>\d+)<<(?<geoHashCode>\w+)$");
+            
+            while (input!="Last note")
+            {
+                bool isValidName = pattern.IsMatch(input);
+                if (!isValidName)
+                {
+                    Console.WriteLine("Nothing found!");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                Match match = pattern.Match(input);
+                string cryptedName = match.Groups["peakName"].Value;
+                int length = int.Parse(match.Groups["length"].Value);
+                string geoHashCode = match.Groups["geoHashCode"].Value;
+
+                string name = "";
+                for (int i = 0; i < cryptedName.Length; i++)
+                {
+                    if (char.IsLetterOrDigit(cryptedName[i]))
+                    {
+                        name += cryptedName[i];
+                    }
+                }
+                if (geoHashCode.Length==length)
+                {
+                    Console.WriteLine($"Coordinates found! {name} -> {geoHashCode}");
+                }
+                else
+                    Console.WriteLine("Nothing found!");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exams/TechnologyFundamentalsC#/FinalExams/FinalExamRn/OnTheWayToAnnapurna/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnTheWayToAnnapurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> stores = new Dictionary<string, List<string>>();
+
+            string[] input = Console.ReadLine().Split("->");
+
+            while (input[0]!="END")
+            {
+                string command = input[0];
+                string nameStore = input[1];
+
+                if (command == "Add")
+                { 
+                    string[] items = input[2].Split(",");
+                    if (!stores.ContainsKey(nameStore))
+                    {
+                        stores.Add(nameStore, new List<string>());
+                    }
+                    foreach (var item in items)
+                    {
+                        stores[nameStore].Add(item);
+                    }
+                }
+                else
+                {
+                    if (stores.ContainsKey(nameStore))
+                        stores.Remove(nameStore);
+                }
+                input = Console.ReadLine().Split("->");
+            }
+
+            Console.WriteLine("Stores list:");
+            foreach (var store in stores.OrderByDescending(x=>x.Value.Count).ThenByDescending(x=>x.Key))
+            {
+                Console.WriteLine(store.Key);
+                foreach (var item in store.Value)
+                {
+                    Console.WriteLine($"<<{item}>>");
+                }
+            }
+        }
+    }
+}
