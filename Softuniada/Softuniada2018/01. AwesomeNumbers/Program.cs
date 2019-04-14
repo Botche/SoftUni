000798c31b2ff0075db750aead7abeb6a259333c
Botche <gabi_petkov@abv.bg>
2019-04-14 12:21:11 +0300

    Added softuniada

---
+++ Softuniada/Softuniada2018/01. AwesomeNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AwesomeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int numberToDivide = int.Parse(Console.ReadLine());
+
+            int level = 0;
+            if(Math.Abs(number)%2==1)
+            {
+                level++;
+            }
+
+            if(number<0)
+            {
+                level++;
+            }
+
+            if(number%numberToDivide==0)
+            {
+                level++;
+            }
+
+            switch (level)
+            {
+                case 0:
+                    Console.WriteLine("boring");
+                    break;
+                case 1:
+                    Console.WriteLine("awesome");
+                    break;
+                case 2:
+                    Console.WriteLine("super awesome");
+                    break;
+                case 3:
+                    Console.WriteLine("super special awesome");
+                    break;
+            }
+        }
+    }
+}
---
+++ Softuniada/SoftuniadaTraining2019/01. Min3Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Min3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int length = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers.Add(number);
+            }
+            var sortedNumbers = numbers.OrderBy(x => x).ToList();
+            if (numbers.Count < 3)
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    Console.WriteLine(sortedNumbers[i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(sortedNumbers[i]);
+                }
+            }
+        }
+    }
+}
