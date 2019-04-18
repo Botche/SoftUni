dbcd00ee4c0d20419d823e134597f121e4676ade
Botche <gabi_petkov@abv.bg>
2019-04-18 19:49:42 +0300

    Added Web project exercise

---
+++ 02. TechnologyFundamentalsC#/WebProjectExe/Forum/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Forum.Models;
+using Forum.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ForumDbContext context;
+
+        public HomeController(ForumDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = context.Categories
+                .Include(c => c.Author)
+                .Include(c => c.Topics)
+                .ThenInclude(t => t.Author)
+                .ToList();
+
+            List<Topic> topics = context.Topics
+                .Include(t => t.Author)
+                .Include(x => x.Comments)
+                .ThenInclude(c => c.Author)
+                .OrderByDescending(x => x.CreatedDate)
+                .ThenByDescending(x => x.LastUpdatedDate)
+                .ToList();
+
+            ViewData["Categories"] = categories;
+
+            return View(topics);
+        }
+    }
+}
---
+++ 02. TechnologyFundamentalsC#/WebProjectExe/Forum/Data/ForumDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Forum.Models;
+
+namespace Forum.Data
+{
+    public class ForumDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Topic> Topics { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=TopicsDb;Integrated Security=true;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
