f6ff7ccb94e2a4f4fadbb69316fa926f943d7767
Botche <gabi_petkov@abv.bg>
2019-06-13 18:36:30 +0300

    Added Generics

---
+++ 02. C#Advanced/70. Generics/01. BoxOfT/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> box;
+
+        public int Count { get => BoxOfT.Count; }
+        public List<T> BoxOfT { get; set; }
+
+        public Box()
+        {
+            BoxOfT = new List<T>();
+        }
+        public void Add(T element)
+        {
+            BoxOfT.Add(element);
+        }
+
+        public T Remove()
+        {
+            var lastElement = BoxOfT.LastOrDefault();
+
+            if (BoxOfT.Count!=0)
+            {
+                BoxOfT.RemoveAt(Count - 1);
+            }
+            else
+            {
+                throw new InvalidOperationException("Box is empty");
+            }
+
+            return lastElement;
+        }
+    }
+}
---
+++ 02. C#Advanced/70. Generics/03. GenericScale/EqualityScale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : IComparable<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left,T rigth)
+        {
+            this.left = left;
+            this.right = rigth;
+        }
+
+        public bool AreEqual()
+        {
+            bool result = this.left.Equals(this.right);
+
+            return result;
+
+        }
+    }
+}
