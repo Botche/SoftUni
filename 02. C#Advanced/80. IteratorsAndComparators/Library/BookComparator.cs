9b37f5fc5fd1ee2b8afcd265813a71a5543ce0d4
Botche <gabi_petkov@abv.bg>
2019-06-17 15:08:08 +0300

    Added Iterators And Comparators

---
+++ 02. C#Advanced/80. IteratorsAndComparators/Library/BookComparator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book x, Book y)
+        {
+            var titleDiff = x.Title.CompareTo(y.Title);
+
+            if (titleDiff != 0)
+            {
+                return titleDiff;
+            }
+
+            return y.Year.CompareTo(x.Year);
+        }
+    }
+}
---
+++ 02. C#Advanced/80. IteratorsAndComparators/Library/Library.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+        
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+
+            //for (int i = 0; i < this.books.Count; i++)
+            //{
+            //    yield return this.books[i];
+            //}
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly IList<Book> data;
+            private int currentIndex;
+
+            public LibraryIterator(IEnumerable<Book> data)
+            {
+                this.Reset();
+                this.data = new List<Book>(data);
+            }
+
+            public Book Current => this.data[currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext() => ++this.currentIndex < this.data.Count;
+
+            public void Reset() => this.currentIndex = -1;
+        }
+    }
+}
