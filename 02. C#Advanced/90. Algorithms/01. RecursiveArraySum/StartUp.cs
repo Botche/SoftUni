ae9ddab8bae09883f41fdfe4fe1cd22ca320f86d
Botche <gabi_petkov@abv.bg>
2019-06-30 12:49:09 +0300

    Added Basic Algorithms

---
+++ 02. C#Advanced/90. Algorithms/01. RecursiveArraySum/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = RecursiveArraySum(arr, 0);
+
+            Console.WriteLine(sum);
+        }
+
+        private static int RecursiveArraySum(int[] arr, int index)
+        {
+            if (index == arr.Length)
+            {
+                return 0;
+            }
+
+            int result = arr[index] + RecursiveArraySum(arr, ++index);
+
+            return result;
+        }
+    }
+}
---
+++ 02. C#Advanced/90. Algorithms/07. BinarySearch/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int key = int.Parse(Console.ReadLine());
+
+            if (input == String.Empty)
+            {
+                return;
+            }
+
+            int[] arr = input
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int index = IndexOf(arr, key);
+            Console.WriteLine(index);
+        }
+        public static int IndexOf(int[] arr, int key)
+        {
+            int lo = 0;
+            int hi = arr.Length - 1;
+
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+
+                if (key < arr[mid])
+                {
+                    hi = mid - 1;
+                }
+                else if (key > arr[mid])
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
