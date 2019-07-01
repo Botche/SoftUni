8b44627c37eb826b633d3e12858476c680ac2f91
Botche <gabi_petkov@abv.bg>
2019-07-02 00:50:43 +0300

    Starting course C#OOP and Added WorkingWithAbstraction

---
+++ 03. C#OOP/00. WorkingWithAbstraction/01. RhombusOfStars/RhombusOfStars.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._RhombusOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfRows = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= countOfRows; i++)
+            {
+                printRow(i, countOfRows);
+            }
+
+            for (int i = countOfRows - 1; i >= 1; i--)
+            {
+                printRow(i, countOfRows);
+            }
+        }
+
+        private static void printRow(int stars, int countOfRows)
+        {
+            int leadingSpaces = countOfRows - stars;
+
+            Console.Write(new string(' ', leadingSpaces));
+
+            for (int star = 0; star < stars; star++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
