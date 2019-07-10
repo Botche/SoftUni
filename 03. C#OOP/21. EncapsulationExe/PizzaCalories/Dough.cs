67e20f6f7e7eeb929b56e9db5a84a366b4cfe83a
Botche <gabi_petkov@abv.bg>
2019-07-10 20:43:01 +0300

    Added Encapsulation Exercises

---
+++ 03. C#OOP/21. EncapsulationExe/PizzaCalories/Dough.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+        private string flourType;
+        private string bakingTechnique;
+        private int weight;
+        private double flourModifier;
+        private double bakingModifier;
+
+        public Dough(string flourType, string backingTechnique, int weight)
+        {
+            this.FlourType = flourType;
+            this.BakingTechnique = backingTechnique;
+            this.Weight = weight;
+        }
+
+        public string FlourType
+        {
+            get => this.flourType;
+            private set
+            {
+                if (value.ToLower() == "white")
+                {
+                    flourModifier = 1.5;
+                }
+                else if (value.ToLower() == "wholegrain")
+                {
+                    flourModifier = 1.0;
+                }
+                else
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidTypeOfDough);
+                }
+                this.flourType = value;
+            }
+        }
+
+        public string BakingTechnique
+        {
+            get => this.bakingTechnique;
+            private set
+            {
+                if (value.ToLower() == "crispy")
+                {
+                    bakingModifier=0.9;
+                }
+                else if (value.ToLower() == "chewy")
+                {
+                    bakingModifier = 1.1;
+                }
+                else if (value.ToLower() == "homemade")
+                {
+                    bakingModifier = 1.0;
+                }
+                else
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidTypeOfDough);
+                }
+                bakingTechnique = value;
+            }
+        }
+
+        public int Weight
+        {
+            get => this.weight;
+            private set
+            {
+                if (value<1 || value>200)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidDoughWeight);
+                }
+                this.weight = value;
+            }
+        }
+
+        public double CalculateDough()
+        {
+            double sumOfDough = 0;
+
+            sumOfDough = 2 * Weight * flourModifier * bakingModifier;
+
+            return sumOfDough;
+        }
+    }
+}
---
+++ 03. C#OOP/21. EncapsulationExe/PizzaCalories/Pizza.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Pizza
+    {
+        private string name;
+        private Dough dough;
+        private List<Topping> toppings;
+        
+
+        public Pizza(string name, Dough dough)
+        {
+            this.Name = name;
+            this.dough = dough;
+            this.toppings = new List<Topping>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (value.Length<1 || value.Length>15)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidPizzaNameLength);
+                }
+                this.name = value;
+            }
+        }
+        public void AddTopping(Topping topping)
+        {
+            if (toppings.Count==10)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidToppingsCount);
+            }
+            toppings.Add(topping);
+        }
+
+        public override string ToString()
+        {
+            double sumOfPizzaCalories = 0;
+
+            foreach (var topping in toppings)
+            {
+                sumOfPizzaCalories += topping.CalculateTopping();
+            }
+
+            sumOfPizzaCalories += dough.CalculateDough();
+
+            return $"{Name} - {sumOfPizzaCalories:f2} Calories.";
+        }
+    }
+}
