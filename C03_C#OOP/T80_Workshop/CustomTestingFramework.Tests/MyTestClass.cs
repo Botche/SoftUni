137b11c6911efe0f3ef3250549e48b6abccb17e6
Botche <gabi_petkov@abv.bg>
2019-09-04 16:28:29 +0300

    Added Theme Workshop

---
+++ C03_C#OOP/T80_Workshop/CustomTestingFramework.Tests/MyTestClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using CustomTestingFramework.Asserts;
+using CustomTestingFramework.Attributes;
+
+namespace CustomTestingFramework.Tests
+{
+    [TestClass]
+    public class MyTestClass
+    {
+        [TestMethod]
+        public void TestSumValues()
+        {
+            int a = 2;
+            int b = 3;
+
+            int expectedResult = 5;
+            int result = a + b;
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestSumValues2()
+        {
+            int a = 2;
+            int b = 3;
+
+            int expectedResult = 6;
+            int result = a + b;
+
+            Assert.AreNotEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestSumValues3()
+        {
+            var a = new string[5];
+
+            Assert.Throws<IndexOutOfRangeException>(
+                () => a[12] == "LALala");
+        }
+    }
+}
---
+++ C03_C#OOP/T80_Workshop/CustomTestingFramewrok/TestRunner/Runner.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+
+using CustomTestingFramework.Utilities;
+using CustomTestingFramework.Attributes;
+using CustomTestingFramework.Exceptions;
+using CustomTestingFramework.TestRunner.Contracts;
+
+namespace CustomTestingFramework.TestRunner
+{
+    public class Runner : ITestRunner
+    {
+        private readonly ICollection<string> resultInfo;
+
+        public Runner()
+        {
+            resultInfo = new List<string>();
+        }
+
+        public ICollection<string> Run(string assemblyPath)
+        {
+            var testClasses = Assembly
+                .LoadFrom(assemblyPath)
+                .GetTypes()
+                .Where(t => t.HasAttribute<TestClassAttribute>())
+                .ToList();
+
+            foreach (var testClass in testClasses)
+            {
+                var testMethods=testClass
+                    .GetMethods()
+                    .Where(m => m.HasAttribute<TestMethodAttribute>())
+                    .ToList();
+
+                var testClassInstance = Activator.CreateInstance(testClass);
+
+                foreach (var testMethod in testMethods)
+                {
+                    try
+                    {
+                        testMethod.Invoke(testClassInstance, null);
+
+                        resultInfo.Add($"Method: {testMethod.Name} - passed!");
+                    }
+                    catch (TestException)
+                    {
+                        resultInfo.Add($"Method: {testMethod.Name} - failed!");
+                    }
+                    catch
+                    {
+                        resultInfo.Add($"Method: {testMethod.Name} - unexpected error occured!");
+                    }
+                }
+            }
+
+            return resultInfo;
+        }
+    }
+}
