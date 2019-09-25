384326ee04be72db32c545f7ab87c23e71406a12
Botche <gabi_petkov@abv.bg>
2019-09-25 12:03:41 +0300

    Solved Third Problem

--- C04_C#Databases/T20_BuiltInFunctionsExercises/BuiltInFunctions.sql
+++ C04_C#Databases/T20_BuiltInFunctionsExercises/BuiltInFunctions.sql
@@ -5,3 +5,8 @@ WHERE FirstName LIKE 'SA%'
 SELECT FirstName, LastName
 FROM Employees
 WHERE LastName LIKE '%ei%'
+
+SELECT FirstName 
+FROM Employees
+WHERE DepartmentId IN (3, 10)
+AND DATEPART(YEAR, HireDate) BETWEEN 1995 AND 2005;
