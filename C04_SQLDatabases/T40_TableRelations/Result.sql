748ead25d5ac59ca71509c67c2dfb1795b745616
Botche <gabi_petkov@abv.bg>
2019-10-01 22:35:47 +0300

    Solved second problem

--- C04_SQLDatabases/T40_TableRelations/Result.sql
+++ C04_SQLDatabases/T40_TableRelations/Result.sql
@@ -1,7 +1,7 @@
 -- Problem 1
-CREATE DATABASE OneToOne;
+CREATE DATABASE Relations;
 
-USE OneToOne;
+USE Relations;
 
 CREATE TABLE Passports
 (
@@ -30,3 +30,31 @@ INSERT INTO Persons([FirstName], [Salary], [PassportID]) VALUES
 SELECT *
   FROM Persons AS p
   JOIN Passports As pass ON pass.PassportID = p.PassportID;
+
+-- Problem 2
+CREATE TABLE Manufacturers(
+	ManufacturerID INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(30) NOT NULL,
+	EstablishedOn DATE NOT NULL
+);
+
+CREATE TABLE Models(
+	ModelID INT PRIMARY KEY NOT NULL,
+	[Name] VARCHAR(30) NOT NULL,
+	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID) NOT NULL
+);
+
+INSERT INTO Manufacturers ([Name], EstablishedOn)
+VALUES ('BMW', '07/03/1916'),
+		('Tesla', '01/01/2003'),
+		('Lada', '01/05/1966');
+
+INSERT INTO Models (ModelID, [Name], ManufacturerID)
+VALUES (101, 'X1', 1),
+		(102, 'I6', 1),
+		(103, 'Model S', 2),
+		(104, 'Model X', 2),
+		(105, 'Model 3', 2),
+		(106, 'Nova', 3);
+
+SELECT * FROM Models;
