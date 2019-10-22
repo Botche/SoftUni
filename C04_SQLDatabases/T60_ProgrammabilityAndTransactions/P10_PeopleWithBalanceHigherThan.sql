5d1b679ded24814c4468e3eb4e9a52f627e5f66d
Botche <gabi_petkov@abv.bg>
2019-10-22 17:59:13 +0300

    Solved all problems

--- C04_SQLDatabases/T60_ProgrammabilityAndTransactions/P10_PeopleWithBalanceHigherThan.sql
+++ C04_SQLDatabases/T60_ProgrammabilityAndTransactions/P10_PeopleWithBalanceHigherThan.sql
@@ -3,5 +3,6 @@ AS
 	SELECT ah.FirstName, ah.LastName
 	FROM Accounts AS a
 	JOIN AccountHolders AS ah ON ah.Id = a.AccountHolderId
-	WHERE SUM(a.Balance) > @balance
-	GROUP BY  ah.FirstName, ah.LastName;
+	GROUP BY  ah.FirstName, ah.LastName
+	HAVING SUM(a.Balance) > @balance
+	ORDER BY ah.FirstName, ah.LastName;
